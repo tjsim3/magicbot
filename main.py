d74d1859fc0386e617673eaa38a4668dc992909f
tjsim3 <tjsimmons823@gmail.com>
2025-09-14 14:13:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ intents.members = True  # Enable if you need member-related events
 
 # Create bot instance
 bot = commands.Bot(command_prefix='%', intents=intents)
+await setup_events(bot)
 bot.start_time = time.time()
 
 
